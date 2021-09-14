9a6bdb223da41da8b15241320a17058509a0192f
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2021-09-13 21:56:13 -0300

    :heavy_plus_sign: Adicionando branch
    Busca Linear Sequencial, aplicação para atividade de algoritmos.

---
+++ buscaLinearSequencial.py
@@ -0,0 +1,50 @@
+from random import randint
+import time
+
+def esperar():
+	pausa = time.sleep(1)
+	return pausa
+
+def painel():
+	print('\n\n\t\t\tBusca Linear')
+	print('Análise de Algoritmos - Rodrigo Hagstrom')
+	esperar()
+	print('\n\nGabriel de Jesus\nNatã Ferreira\nRafhael Martins\nVictor Resende\n\n')
+	esperar()
+	print('Gerando vetores com dez dados aleatórios.')
+	esperar()
+	print('Gerando vetores com dez dados aleatórios..')
+	esperar()
+	print('Gerando vetores com dez dados aleatórios...')
+	
+def criarVetor():
+	vetor = []
+	for i in range(10):
+		vetor.append(randint(0, 20))
+	return vetor
+	
+def buscaLinear():
+	vetor = criarVetor()
+	esperar()
+	print("Vetor gerado: ", vetor)
+	print('\n')
+
+	esperar()
+	desejado = int(input('Digite um número para procurar na lista: '))
+	print('\n')
+	
+	for n, x in enumerate(vetor):
+		esperar()
+		print(f'Verificando se o número {desejado} está no índice {n} da lista.')
+		if x == desejado:
+			esperar()
+			print(f'O número {desejado} foi encontrado no índice {n} da lista.\n\n')
+		else:
+			esperar()
+			print('Número não encontrado.\n')
+
+	esperar()
+	print('Fim da Busca Linear.\nObrigado por usar!')
+	
+painel()
+buscaLinear()
