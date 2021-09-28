47d1a9e3f8d5eec40ff172a5067c735fb2d246a7
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2021-09-27 21:12:12 -0300

    Ajustando últimos detalhes
    Ajustando últimos detalhes para apresentação.

--- buscaLinear.py
+++ buscaLinear.py
@@ -10,21 +10,21 @@ def painel():
 	print('Gerando vetores com dez dados aleatórios...')
 	
 def criarVetor():
-	vetor = []
-	for i in range(35000):
-		vetor.append(randint(0, 1000))
+	vetor = list(range(0, 100000, 5))
+
+	#for i in range(3500000):
+	#	vetor.append(randint(0, 100000))
 	return vetor
 
-
-print('\n\n\n######################################################')
-print('\t\tBUSCA LINEAR SEQUENCIAL')
-print('######################################################')
 	
 def buscaLinear():
-	vetor = criarVetor()
+	print('\n\n\n######################################################')
+	print('\t\tBUSCA LINEAR SEQUENCIAL')
+	print('######################################################')
+
 	#print("Vetor gerado: ", vetor)
 	print('\n')
-	desejado = int(input('Digite um número para procurar na lista: '))
+	desejado = num_escolhido
 	print('\n')
 	
 	for n, x in enumerate(vetor):
@@ -32,25 +32,18 @@ def buscaLinear():
 		if x == desejado:
 			print(f'O número {desejado} foi encontrado no índice {n} da lista.\n\n')
 
+	#print(f'O número {desejado} não foi encontrado na lista.\n\n')
 	print('Fim da Busca Linear Sequencial.\nObrigado por usar!')
 	
-painel()
-
-inicio_buscaLinear = time.time()
-buscaLinear()
-fim_buscaLinear = time.time()
-print('Tempo execucao:', (fim_buscaLinear - inicio_buscaLinear)/60)
-
-print('\n\n\n######################################################')
-print('\t\tBUSCA LINEAR MELHORADA')
-print('######################################################')
-	
 def buscaLinearMelhorada():
-	vetor = criarVetor()
+	print('\n\n\n######################################################')
+	print('\t\tBUSCA LINEAR MELHORADA')
+	print('######################################################')
+
 	#print("Vetor gerado: ", vetor)
 	print('\n')
 
-	desejado = int(input('Digite um número para procurar na lista: '))
+	desejado = num_escolhido
 	print('\n')
 	
 	for n, x in enumerate(vetor):
@@ -59,54 +52,54 @@ def buscaLinearMelhorada():
 		if x == desejado:
 			print(f'O número {desejado} foi encontrado no índice {n} da lista.\n\n')
 			break
-		else:
-			print('Número não encontrado.\n')
-
+	#print(f'O número {desejado} não foi encontrado na lista.\n\n')
 	print('Fim da Busca Linear Melhorada.\nObrigado por usar!')
-	
+
+def buscaLinearSentinela():
+    print('\n\n\n######################################################')
+    print('\t\tBUSCA LINEAR COM SENTINELA')
+    print('######################################################')
+
+    #print("Vetor gerado: ", vetor)
+    print('\n')
+
+
+    sentinela = num_escolhido
+    print('\n')
+    num = len(vetor)
+    ultimo = vetor[num - 1]
+    vetor[num - 1] = sentinela
+    i = 0
+
+    while vetor[i] != sentinela:
+        i += 1
+        #print(f'Verificando se o número {sentinela} está no índice {i} da lista.')
+
+    vetor[num - 1] = ultimo
+
+    if i == (len(vetor) - 1) or vetor[num - 1] == sentinela:
+        print('Número não encontrado.\n')
+
+
+    print(f'O número {sentinela} foi encontrado no índice {i} da lista.\n\n')
+    print('Fim da Busca Linear com Sentinela.\nObrigado por usar!')
+
+
 painel()
+num_escolhido = int(input('Digite um número para procurar na lista: '))
+vetor = criarVetor()
+
+inicio_buscaLinear = time.time()
+buscaLinear()
+fim_buscaLinear = time.time()
+print('Tempo execucao:', (fim_buscaLinear - inicio_buscaLinear))
 
 inicio_buscaLinearMelhorada = time.time()
 buscaLinearMelhorada()
 fim_buscaLinearMelhorada = time.time()
-print('Tempo execucao:', (fim_buscaLinearMelhorada - inicio_buscaLinearMelhorada)/60)
-
-print('\n\n\n######################################################')
-print('\t\tBUSCA LINEAR COM SENTINELA')
-print('######################################################')
-
-
-def buscaLinearSentinela():
-	vetor = criarVetor()
-	#print("Vetor gerado: ", vetor)
-	print('\n')
-
-
-	sentinela = int(input('Digite um número para procurar na lista: '))
-	print('\n')
-	resultado = -1
-	num = len(vetor)
-	ultimo = vetor[num - 1]
-	vetor[num - 1] = sentinela
-	i = 0
-
-	while vetor[i] != sentinela:
-		i += 1
-		#print(f'Verificando se o número {sentinela} está no índice {i} da lista.')
-
-	vetor[num - 1] = ultimo
-
-	if i == len(vetor) - 1:
-		print('Número não encontrado.\n')
-
-
-	print(f'O número {sentinela} foi encontrado no índice {i} da lista.\n\n')
-
-	print('Fim da Busca Linear com Sentinela.\nObrigado por usar!')
-
-painel()
+print('Tempo execucao:', (fim_buscaLinearMelhorada - inicio_buscaLinearMelhorada))
 
 inicio_buscaLinearSentinela = time.time()
 buscaLinearSentinela()
 fim_buscaLinearSentinela = time.time()
-print('Tempo execucao:', (fim_buscaLinearSentinela - inicio_buscaLinearSentinela)/60)
+print('Tempo execucao:', (fim_buscaLinearSentinela - inicio_buscaLinearSentinela))
